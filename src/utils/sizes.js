be2e67424fa79a1027ce11a24fccbf09532eca1f
Hitomi Egusa <hitomiegusa@Hitomi.local>
2023-09-04 00:41:51 +0900

    initial

---
+++ src/utils/sizes.js
@@ -0,0 +1,17 @@
+export const fontSizes = {
+  sm: 8,
+  md: 10,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+  xxxl: 80,
+};
+
+export const spacing = {
+  sm: 8,
+  md: 10,
+  lg: 24,
+  xl: 32,
+  xxl: 40,
+  xxxl: 80,
+};
